93fe10d2860e22cf8b8fd80402f4b0b229587a32
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-01-19 03:02:43 -0500

    Tweak number validator to account for more exclusions, add object enumeration.

--- assets/script.js
+++ assets/script.js
@@ -13,17 +13,13 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
 let sPassSource = "";
 let rgUserChoice = [];
 
-// console.log(objCharacterPool.lowercase[8-1]);
 const objCharacterPool = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
-  numbers: "1234567890",
-  symbols: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\'"
+  number: "1234567890",
+  special: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\'"
 };
 objCharacterPool.uppercase= objCharacterPool.lowercase.toUpperCase();
 
@@ -33,29 +29,44 @@ function generatePassword() {
   let nPassLength = 0;
   nPassLength = prompt("How many characters should the password have? (8-128)");
   switch (true) {
-      case (!IsANumber(nPassLength)):
-        
-        {alert("That is not a number. Please begin the process again by clicking the Generate Password button.");}
+      case (!IsANumber(nPassLength)):    
+        {alert("'"+nPassLength+"' is not a valid number. Please begin the process again by clicking the Generate Password button.");}
         return "The user has failed to request a valid password length."
       case (nPassLength > 128):
-        {alert("The maximum password length is 128 characters, and you have selected "+Number(nPassLength)+". Please begin the process again by clicking the Generate Password button.");}
+        {alert("The maximum password length is 128 characters, and you have entered "+Number(nPassLength)+". Please begin the process again by clicking the Generate Password button.");}
         return "The requested password was too long."
       break;
       case (nPassLength < 8):  
-        { alert("The minimum password length is 8 characters, and you have selected "+Number(nPassLength)+". Please begin the process again by clicking the Generate Password button.")}
+        { alert("The minimum password length is 8 characters, and you have entered "+Number(nPassLength)+". Please begin the process again by clicking the Generate Password button.")}
         return "The requested password was too short."
       break;
   }
+  for (property in objCharacterPool) {
+    if(confirm("Shall we add "+ property +" characters to the pool of characters to draw from? Select OK to approve or Cancel to exclude "+ property +" characters.")) {
+      rgUserChoice.push(property);
+    }
+  }
+  console.log (rgUserChoice);
+  
   return nPassLength;
 
 }
 
+//function RandomFromPool(inputPool) {
+      // for number of elements in array, add the .element from object, hopefully elegantly.
+
+      // if not, you must select at least one type of character (# # # #) to generate the password
+      // from. PLease try again later, etc.
+
+      // will add a validator for at least one of selected categories.
+
+// }
 
 function IsANumber(suppliedString) { //This function validates input as a number.
   let sValidString=true;
-  let strLIndex=0;
-  
-  if (suppliedString===undefined && suppliedString==="") {sValidString=false; }
+  let strLIndex=0; 
+  if (suppliedString==undefined || suppliedString==="") {sValidString=false; }
+  // getting rid of the usual suspects
   else
     {
       console.log("indexNum: "+ strLIndex);
@@ -63,12 +74,14 @@ function IsANumber(suppliedString) { //This function validates input as a number
           // We check every character in the supplied string for membership in the digit set,
           // which is already conveniently present in objCharacterPool.numbers property.
           
-          sValidString=false;
+          sValidString=false; 
           
-         for (numSetIndex=0; numSetIndex<objCharacterPool.numbers.length; numSetIndex++) {
-            if (suppliedString[strLIndex] == objCharacterPool.numbers[numSetIndex]) {
+         for (numSetIndex=0; numSetIndex<objCharacterPool.number.length; numSetIndex++) {
+            if (suppliedString[strLIndex] == objCharacterPool.number[numSetIndex]) {
               console.log("indexNum: "+ strLIndex+"numSetIndex: "+numSetIndex);
-              sValidString=true; //if there is at least one match, the digit is validated.
+              sValidString=true; //if there is at least one match, that character is validated
+                                 //as a digit, and the loop continues until all characters have been checked.
+                                 //Otherwise, if sValidString remains false, the while loop ends.
               
             } 
          }
