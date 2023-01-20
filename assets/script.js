e90b653b66bb15d2b67e28ceffa5a714d97c8a8f
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-01-19 22:33:25 -0500

    Remove testing code. Include additional clarifying comments.

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+// STUDENT CODE BEGINS
 let sPassSource = "";
 let nFinalPass=0;
 let rgUserChoice = [];
 
+/* Create object in which to store all available character sets. 
+Additional properties may be added, and the code will automatically include them in user options. */
 const objCharacterPool = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   number: "1234567890",
@@ -22,11 +24,11 @@ const objCharacterPool = {
 };
 objCharacterPool.uppercase= objCharacterPool.lowercase.toUpperCase();
 
+// Main function.
 function generatePassword() {
-
-  
   let nPassLength = 0;
   nPassLength = prompt("How many characters should the password have? (8-128)");
+  // Check for anomalies. Display anomalous outcomes in the password box.
   switch (true) {
       case (!IsANumber(nPassLength)):
         {alert("'"+nPassLength+"' is not a valid number. Please begin the process again by clicking the Generate Password button.");}
@@ -40,6 +42,7 @@ function generatePassword() {
         return "The requested password was too short."
       break;
   }
+  // Collect user choices.
   rgUserChoice=[];
   for (property in objCharacterPool) {
     if(confirm("Shall we add "+ property +" characters to the pool of characters to draw from? Select OK to approve or Cancel to exclude "+ property +" characters.")) {
@@ -51,52 +54,50 @@ function generatePassword() {
     return "Cannot generate password. The user has rejected all possible characters."
     }
   
-  GeneratePool();  // Generate pool of numbers.
-
-  nFinalPass = RandomFromPool(sPassSource, nPassLength);
+ GeneratePool();
+ nFinalPass = RandomFromPool(sPassSource, nPassLength);
+ // The following loop tests the generated password and regenerates it 
+ // if it fails to include at least one character from each criterion
+ // approved by the user.
  while (!ValidatePassword(nFinalPass)){
   nFinalPass="";
   nFinalPass=RandomFromPool(sPassSource,nPassLength);
  }
-    
-  
-  console.log("validation..."+ValidatePassword(nFinalPass));
-
     return nFinalPass;
 }
 
 function GeneratePool() {
-    // generates the pool of possible characters based on the user's choices
+    // Generate the pool of possible characters based on the user's choices.
     sPassSource="";
     for (i=0;i<rgUserChoice.length;i++) {sPassSource=sPassSource.concat(objCharacterPool[rgUserChoice[i]]);}  
-      //adding values from only those properties of the objCharacterPool object that correspond to
-      //user criteria, which are stored in the rgUserChoice array.
+      //Only those properties of the objCharacterPool object that correspond to
+      //user criteria are considered, and their values added to the character pool.
+      //The rgUserChoice array stores user choices.
                                                                     
     return;
 }
     function ValidatePassword (candidatePass) {
-      let i=0;
-      let y=0;
-      let x=0;
-      let wherematch=0;
-      let foundFlag=false;
-      while (y<rgUserChoice.length) {
-        
-        console.log("Now traversing"+ rgUserChoice[y]);
-        
-        for (x=0;x<objCharacterPool[rgUserChoice[y]].length;x++) {
-          console.log("Now traversing the actual string: "+ objCharacterPool[rgUserChoice[y]]);
-            if (candidatePass.indexOf(objCharacterPool[rgUserChoice[y]][x])===-1) {
-              console.log("Not found "+objCharacterPool[rgUserChoice[y]][x]+" in "+candidatePass);
+      /*The password is validated to ensure that at least one character from each category
+      approved by the user is included in the final password.*/
+
+      let choiceIndex=0; // Index to traverse the user criteria.
+      let currCharIndex=0; //Index to traverse all characters inside each criterion.
+      let foundFlag=false; //
+      while (choiceIndex<rgUserChoice.length) {     
+        // Traversing across criteria.   
+        for (currCharIndex=0;currCharIndex<objCharacterPool[rgUserChoice[choiceIndex]].length;currCharIndex++) { 
+          //Traversing character by character within the current criterion.
+            if (candidatePass.indexOf(objCharacterPool[rgUserChoice[choiceIndex]][currCharIndex])===-1) {
+              foundFlag=false;
             } else {foundFlag=true;}
           }
-          if (!foundFlag) {console.log("Not found "+objCharacterPool[rgUserChoice[y]]+" in "+candidatePass);return false;} else {console.log(objCharacterPool[rgUserChoice[y]]+" cleared.")}
-          y++; 
+          if (!foundFlag) {return false;} else {console.log(objCharacterPool[rgUserChoice[choiceIndex]]+" cleared.")}
+           //If even one category fails to be represented, we may break out of the function; the password is ruined.
+          choiceIndex++; 
           foundFlag=false;
         }
         return true;
       }
-  
 
 function RandomFromPool(inputPool,strLength) {
   console.log(inputPool+" at "+strLength);
@@ -110,7 +111,7 @@ function RandomFromPool(inputPool,strLength) {
     return nTempRandomisedPass;
 }
 
- function IsANumber(suppliedString) { //This function validates input as a number.
+ function IsANumber(suppliedString) { //This function validates input as a positive integer.
   let sValidString=true;
   let strLIndex=0; 
   if (suppliedString==undefined || suppliedString==="") {sValidString=false; }
@@ -129,7 +130,7 @@ function RandomFromPool(inputPool,strLength) {
                                   Otherwise, if sValidString remains false, the while loop ends.*/
             } 
          }
-         strLIndex++ //incrementing the supplied string traversal loop
+         strLIndex++ //Increment the suppliedString traversal loop.
       }
     }
     return sValidString;
